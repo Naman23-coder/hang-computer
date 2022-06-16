5af98379acdb163fe4e54dc4c311e9c479925d2e
Naman Sharma <namansharma232009@gmail.com>
2022-06-16 20:13:14 +0530

    Update hang.py

--- hang.py
+++ hang.py
@@ -1,3 +1,5 @@
 import requests
 
-pi = requests.get('https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt')
+pi = requests.get('https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt').text
+while True:
+   print(pi*24578855885588657875886)
